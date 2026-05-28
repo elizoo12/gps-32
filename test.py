021d5032b6c0d315fa7dd4b30807e3794d3fa3aa
Eric_HC <ericheca11@gmail.com>
2026-05-28 14:48:15 +0200

    docstrings on first line

--- test.py
+++ test.py
@@ -9,19 +9,22 @@ class TestStringMethods(unittest.TestCase):
     """clase para probar los metodos de string transforms"""
 
     def test_is_upper(self):
+        """prueba to_upper_case"""
         sting = transform.to_upper_case("hello")
         self.assertEqual(sting, "HELLO")
-    """prueba to_upper_case"""
+
 
     def test_is_lower(self):
+        """prueba to_lower_case"""
         sting = transform.to_lower_case("HELLO")
         self.assertEqual(sting, "hello")
-    """prueba to_lower_case"""
+
 
     def test_is_capitalize(self):
+        """prueba to_capitalize"""
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
-    """prueba to_capitalize"""
+
 
 
 if __name__ == '__main__':
